0ec45cd7beac1d0cdc134ca7f7a83bb9ff8e0092
fbarragino <fbarra.gino@gmail.com>
2026-02-23 23:42:24 -0300

    Cambio en rama Titulo con el name Gino

--- app/src/main/java/com/example/app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppkotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Gino",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
