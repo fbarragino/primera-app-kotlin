08d519edc2d86ecc987ed8593e3f44bcd413ec5a
fbarragino <fbarra.gino@gmail.com>
2026-02-24 19:52:06 -0300

    Actualizado en clases

--- app/src/main/java/com/example/app_kotlin/trivia/QuizViewModel.kt
+++ app/src/main/java/com/example/app_kotlin/trivia/QuizViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.app_kotlin.trivia
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class QuizViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(
+        value = QuizUiState(
+
+        )
+
 
 }
